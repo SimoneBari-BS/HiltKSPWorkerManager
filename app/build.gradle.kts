12396cf26a50b1a782dd109f90e269f602c38e07
Simone Bari <simb@bendingspoons.com>
2023-09-04 10:55:44 +0200

    use ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
 }
 
 
@@ -61,6 +63,15 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
+
+    implementation("com.google.dagger:hilt-android:2.48")
+
+    implementation("androidx.hilt:hilt-common:1.0.0")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+
+    ksp("com.google.dagger:hilt-android-compiler:2.48")
+    ksp("androidx.hilt:hilt-compiler:1.0.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
